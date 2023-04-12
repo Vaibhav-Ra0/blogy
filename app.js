dceb52978e9619033516871704eab69088a45cb3
vaibhav rao <me.vaibhavrao833@gmail.com>
2023-04-13 02:30:25 +0530

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+
+  res.sendFile(__dirname+"/index.html");
+
+})
+
+app.listen(3000,function(req,res){
+    console.log("listining at port : 3000");
+})
